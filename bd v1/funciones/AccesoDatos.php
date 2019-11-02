77d8babe2be7563f8deb97bfb58f802e046389bc
octaviovillegas <el.octavio.villegas@gmail.com>
2019-11-02 10:28:42 -0300

    acceso a datos

--- bd v1/funciones/AccesoDatos.php
+++ bd v1/funciones/AccesoDatos.php
@@ -7,7 +7,9 @@ class AccesoDatos
     private function __construct()
     {
         try { 
-            $this->objetoPDO = new PDO('mysql:host=localhost;dbname=istic2019;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $this->objetoPDO = new PDO('mysql:host=remotemysql.com:3306;dbname=0fRgW4a58Y;charset=utf8', '0fRgW4a58Y', 'lccvr8sX6m', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+         
+           // $this->objetoPDO = new PDO('mysql:host=localhost;dbname=istic2019;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             $this->objetoPDO->exec("SET CHARACTER SET utf8");
             } 
         catch (PDOException $e) { 
